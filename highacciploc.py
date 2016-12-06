982e77cd4599be6da5b8f421b8369ca7a900b37c
chenhaiou <chenhaiou@i-vsun.com>
2016-12-06 17:08:37 +0800

    u

--- highacciploc.py
+++ highacciploc.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+# http://lbsyun.baidu.com/index.php?title=webapi/high-acc-ip
+
 ip_addr = raw_input('Your ip:')
 url = 'http://api.map.baidu.com/highacciploc/v1?qcip=%s&qterm=pc&ak=omhNrOPQOYt5iVzNSlZ8MuE6tQC7KxL2&coord=bd09ll&extensions=3' % (ip_addr)
 r = requests.get(url)
